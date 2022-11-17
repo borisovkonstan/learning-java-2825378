050ff82e45601f04f89f6d089bf635957b059d3d
Your Name <you@example.com>
2022-11-17 02:27:39 +0200

    added input and output

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static void announceDeveloperTeatime() {
+        System.out.println("Waitong for tea time....");
+        System.out.println("Type rundom word and press enter!");
+        Scanner input = new Scanner(System.in);
+        input.nextLine();
+        System.out.println("It's developer tea time....");
+    }
+
     public static void main(String[] args) {
+        announceDeveloperTeatime();
 
     }
 
