f54f78b5d0afe0c1c82f61364b49d40d2e1ac1ba
Your Name <you@example.com>
2022-11-17 05:01:33 +0200

    added input and output

--- src/Triangle.java
+++ src/Triangle.java
@@ -4,4 +4,19 @@ public class Triangle {
     double sideLeonOne;
     double sideLeonTwo;
     double sideLeonTree;
+
+    public Triangle(double base, double height,
+                    double sideLeonOne, double sideLeonTwo,
+                    double sideLeonTree) {
+        this.base = base;
+        this.height = height;
+        this.sideLeonOne = sideLeonOne;
+        this.sideLeonTwo = sideLeonTwo;
+        this.sideLeonTree = sideLeonTree;                
+    }
+
+    public double findArea() {
+        return (this.base * this.height) / 2;
+        
+    }
 }
